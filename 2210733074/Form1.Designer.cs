8bb68c80e80f433930e5413c9d81a5e4071f038c
zehrasbr <zehraisbr@gmail.com>
2023-06-27 20:30:55 +0300

    in-time-game
    choose the food never choose the skull

---
+++ 2210733074/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace _2210733074
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dikdörtgenÇizToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çizgileriSilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dikdörtgenÇizToolStripMenuItem,
+            this.çizgileriSilToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem1.Text = "Menü";
+            // 
+            // dikdörtgenÇizToolStripMenuItem
+            // 
+            this.dikdörtgenÇizToolStripMenuItem.Name = "dikdörtgenÇizToolStripMenuItem";
+            this.dikdörtgenÇizToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dikdörtgenÇizToolStripMenuItem.Text = "Dikdörtgen Çiz";
+            this.dikdörtgenÇizToolStripMenuItem.Click += new System.EventHandler(this.dikdörtgenÇizToolStripMenuItem_Click);
+            this.dikdörtgenÇizToolStripMenuItem.MouseMove += new System.Windows.Forms.MouseEventHandler(this.dikdörtgenÇizToolStripMenuItem_MouseMove);
+            // 
+            // çizgileriSilToolStripMenuItem
+            // 
+            this.çizgileriSilToolStripMenuItem.Name = "çizgileriSilToolStripMenuItem";
+            this.çizgileriSilToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.çizgileriSilToolStripMenuItem.Text = "Çizgileri Sil";
+            this.çizgileriSilToolStripMenuItem.MouseMove += new System.Windows.Forms.MouseEventHandler(this.çizgileriSilToolStripMenuItem_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem dikdörtgenÇizToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çizgileriSilToolStripMenuItem;
+    }
+}
+
---
+++ 2210733074/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2210733074
+{
+    public partial class Form1 : Form
+    {
+        int X1, Y1, X2, Y2;
+        string cizimturu;
+        bool ilk;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void çizgileriSilToolStripMenuItem_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Graphics g = this.CreateGraphics();
+                Pen p = new Pen(Color.White, 100);
+                Point nokta1 = new Point(X1, Y1);
+                Point nokta2 = new Point(e.X, e.Y);
+                g.DrawLine(p, nokta1, nokta2);
+                X1 = e.X;
+                Y1 = e.Y;
+            }
+        }
+
+        private void dikdörtgenÇizToolStripMenuItem_MouseMove(object sender, MouseEventArgs e)
+        {
+            cizimturu = "dikdortgen";
+            ilk = false;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (cizimturu == null)
+                {
+                    Graphics g = this.CreateGraphics();
+                    Pen p = new Pen(Color.Blue, 5);
+                    Point nokta1 = new Point(X1, Y1);
+                    Point nokta2 = new Point(e.X, e.Y);
+                    g.DrawLine(p, nokta1, nokta2);
+                    X1 = e.X;
+                    Y1 = e.Y;
+                }
+            }
+        }
+
+        
+   
+        private void dikdörtgenÇizToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cizimturu = "dikdortgen";
+            ilk = false;
+        }
+    }
+}
